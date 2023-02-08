4a12fc6ed442de558de5d96933b25665ea629871
Lasty-progs <archakovk@bk.ru>
2023-02-08 21:56:42 +0300

    add solve

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
- 
-sides = sorted(sides, reverse=True)
- 
-smax = 0
-for i in range(len(sides)):
-   for j in range(i + 1, len(sides)):
-       for k in range(j + 1, len(sides)):
-           a = sides[i]
-           b = sides[j]
-           c = sides[k]
-           if a + b > c and a + c > b and b + c > a:
-               p = (a + b + c) / 2
-               s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-               if s > smax:
-                   smax = s
- 
-print("Максимальная площадь треугольника", smax)
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = b**2 - 4*a*c
+
+if d < 0:
+    print("нет корней")
+elif d == 0:
+    x = -b / (2*a)
+    print(x)
+else:
+    x1 = (-b + d**0.5)/(2*a)
+    x2 = (-b - d**0.5)/(2*a)
+    print(x1,x2)
