291f5e8a4fcc5f3e2c2c80ad09a15deeb467a0ca
Lasty-progs <archakovk@bk.ru>
2023-02-08 23:23:37 +0300

    weather

---
+++ 2/main.py
@@ -0,0 +1,27 @@
+import requests
+
+city = "Moscow,RU"
+appid = "5071b6ba8b9514aa411fe3c5e15a6f3d"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура:", data['main']['temp_max'])
+print("Скорость ветра: <", data['wind']['speed'] ,"> \nВидимость: <", data['visibility'], ">")
+
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Прогноз погоды на неделю:")
+
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
+    print("Скорость ветра <", i['wind']['speed'] ,"> \nВидимость <", i['visibility'], ">")
+    print("____________________________")
