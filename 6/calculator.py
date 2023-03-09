374587286ac20f8a2b9a51a8dc8c27ea3974b9f7
Lasty-progs <archakovk@bk.ru>
2023-03-09 08:25:22 +0300

    complete 6 lab

--- 6/calculator.py
+++ 6/calculator.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton, QMessageBox
 
 class Calculator(QWidget):
     def __init__(self):
         super(Calculator, self).__init__()
 
+        
         self.vbox = QVBoxLayout(self)
         self.hbox_input = QHBoxLayout()
         self.hbox_first = QHBoxLayout()
@@ -13,6 +14,8 @@ class Calculator(QWidget):
         self.hbox_fourth = QHBoxLayout()
         self.hbox_result = QHBoxLayout()
 
+        
+        
         self.vbox.addLayout(self.hbox_input)
         self.vbox.addLayout(self.hbox_first)
         self.vbox.addLayout(self.hbox_second)
@@ -22,6 +25,9 @@ class Calculator(QWidget):
 
         self.input = QLineEdit(self)
         self.hbox_input.addWidget(self.input)
+
+        self.b_0 = QPushButton("0", self)
+        self.hbox_first.addWidget(self.b_0)
 
         self.b_1 = QPushButton("1", self)
         self.hbox_first.addWidget(self.b_1)
@@ -71,14 +77,21 @@ class Calculator(QWidget):
         self.b_result = QPushButton("=", self)
         self.hbox_result.addWidget(self.b_result)
 
+        self.b_clear = QPushButton("Clear", self)
+        self.hbox_result.addWidget(self.b_clear)
+
+
+        #  Button operators connect
         self.b_plus.clicked.connect(lambda: self._operation("+"))
         self.b_minus.clicked.connect(lambda: self._operation("-"))
         self.b_mult.clicked.connect(lambda: self._operation("*"))
         self.b_div.clicked.connect(lambda: self._operation("/"))
-        self.b_float.clicked.connect(lambda: self._operation("."))
 
         self.b_result.clicked.connect(self._result)
+        self.b_clear.clicked.connect(self._clear)
 
+        #  Button numbers connect
+        self.b_0.clicked.connect(lambda: self._button("0"))
         self.b_1.clicked.connect(lambda: self._button("1"))
         self.b_2.clicked.connect(lambda: self._button("2"))
         self.b_3.clicked.connect(lambda: self._button("3"))
@@ -88,18 +101,37 @@ class Calculator(QWidget):
         self.b_7.clicked.connect(lambda: self._button("7"))
         self.b_8.clicked.connect(lambda: self._button("8"))
         self.b_9.clicked.connect(lambda: self._button("9"))
+        self.b_float.clicked.connect(lambda: self._button("."))
+
+    # create MassageBox for errors
+    def warn_display(self,log):
+        err = QMessageBox()
+        if log == 1:
+            err.setText("Please enter the correct value \n May be many dots or text have been typed")
+        elif log == 2:
+            err.setText("Zero division error")
+        err.setWindowTitle("Error")
+        err.exec_()
 
     def _button(self, param):
         line = self.input.text()
         self.input.setText(line + param)
 
     def _operation(self, op):
-        self.num_1 = int(self.input.text())
+        try:
+            self.num_1 = float(self.input.text())
+        except:
+            self.warn_display(1)
+
         self.op = op
         self.input.setText("")
 
     def _result(self):
-        self.num_2 = int(self.input.text())
+        try:
+            self.num_2 = float(self.input.text())
+        except:
+            self.warn_display(1)
+            
         if self.op == "+":
             self.input.setText(str(self.num_1 + self.num_2))
         if self.op == "-":
@@ -107,9 +139,13 @@ class Calculator(QWidget):
         if self.op == "*":
             self.input.setText(str(self.num_1 * self.num_2))
         if self.op == "/":
-            self.input.setText(str(self.num_1 / self.num_2))
-        if self.op == ".":
-            self.input.setText(str(self.num_1) + "." +  str(self.num_2))
+            if self.num_2 != 0.0:
+                self.input.setText(str(self.num_1 / self.num_2))
+            else:
+                self.warn_display(2)
+
+    def _clear(self):
+        self.input.clear()
 
     
 
